4a43c0e8e20e9576fde9a0c2eae2c5d10150f7d0
DamianoCasolari <damianocasolari@gmail.com>
2023-03-17 17:23:37 +0100

    add level javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,31 @@
 const containerEl = document.querySelector(".main_container")
+const selectEl = document.querySelector("select")
+const buttonEl = document.querySelector(".my_btn")
 
 const easy = 100;
 const medium = 81;
 const hard = 49;
 
 
-function difficulty(select_difficulty){
-for (let i = 0; i < select_difficulty; i++) {
-    containerEl.innerHTML += `<div class="cell"></div>`
-}}
+function difficulty(select_difficulty) {
+    for (let i = 1; i <= select_difficulty; i++) {
+        containerEl.innerHTML += `<div class="cell fs-3 d-flex justify-content-center align-items-center">${i}</div>`
+    }
+}
 
-difficulty(easy);
+
+buttonEl.addEventListener("click", function () {
+    containerEl.innerHTML = "";
+    if (selectEl.value == "easy_lv") {
+        difficulty(easy);
+    } else if (selectEl.value == "medium_lv") {
+        difficulty(medium);
+    } else {
+        difficulty(hard);
+    }
+})
+
+
+
+
 
