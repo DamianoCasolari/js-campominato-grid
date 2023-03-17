7fdf2602d0d4f7a20620690cb989b16f3d45928e
DamianoCasolari <damianocasolari@gmail.com>
2023-03-17 19:22:54 +0100

    add change background

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,32 +5,45 @@ const buttonEl = document.querySelector(".my_btn")
 const easy = 100;
 const medium = 81;
 const hard = 49;
-
+let numberElement = 0;
 
 function difficulty(select_difficulty) {
     for (let i = 1; i <= select_difficulty; i++) {
+        numberElement++
         if (selectEl.value == "easy_lv") {
-        containerEl.innerHTML += `<div class="cell fs-3 d-flex justify-content-center align-items-center">${i}</div>`
-        } else if (selectEl.value == "medium_lv"){
-            containerEl.innerHTML += `<div class="cell2 fs-3 d-flex justify-content-center align-items-center">${i}</div>`
+            containerEl.innerHTML += `<div class="single_cell cell  fs-3 d-flex justify-content-center align-items-center">${i}</div>`
+        } else if (selectEl.value == "medium_lv") {
+            containerEl.innerHTML += `<div class="single_cell cell2 fs-3 d-flex justify-content-center align-items-center">${i}</div>`
         } else {
-            containerEl.innerHTML += `<div class="cell3 fs-3 d-flex justify-content-center align-items-center">${i}</div>`
-        }
+            containerEl.innerHTML += `<div class="single_cell cell3 fs-3 d-flex justify-content-center align-items-center">${i}</div>`
     }
-}
-
+}}
 
 buttonEl.addEventListener("click", function () {
     containerEl.innerHTML = "";
     if (selectEl.value == "easy_lv") {
         difficulty(easy);
+        const arrayCell = document.querySelectorAll(".single_cell")
+        changeBackground(arrayCell)
     } else if (selectEl.value == "medium_lv") {
         difficulty(medium);
+        changeBackground()
+        console.log(numberElement);
     } else {
         difficulty(hard);
+        changeBackground()
+        console.log(numberElement);
     }
 })
 
+function changeBackground(arrayElement){
+for (let i = 0; i < arrayElement.length; i++) {
+    const singleCell = arrayElement[i]
+    singleCell.addEventListener("click", function(){
+        this.classList.toggle("click_background")
+        console.log(this)
+    })
+}}
 
 
 
