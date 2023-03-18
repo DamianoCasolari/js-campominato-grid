3e0754f97c73f2f90ae856e3fea75d1dced7bfb1
DamianoCasolari <damianocasolari@gmail.com>
2023-03-18 10:03:34 +0100

    add css for layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,45 +5,65 @@ const buttonEl = document.querySelector(".my_btn")
 const easy = 100;
 const medium = 81;
 const hard = 49;
-let numberElement = 0;
 
 function difficulty(select_difficulty) {
     for (let i = 1; i <= select_difficulty; i++) {
-        numberElement++
         if (selectEl.value == "easy_lv") {
-            containerEl.innerHTML += `<div class="single_cell cell  fs-3 d-flex justify-content-center align-items-center">${i}</div>`
+            containerEl.innerHTML += `<div class="single_cell cell fs-3 d-flex justify-content-center align-items-center">${i}</div>`;
         } else if (selectEl.value == "medium_lv") {
-            containerEl.innerHTML += `<div class="single_cell cell2 fs-3 d-flex justify-content-center align-items-center">${i}</div>`
+            containerEl.innerHTML += `<div class="single_cell cell2 fs-3 d-flex justify-content-center align-items-center">${i}</div>`;
         } else {
-            containerEl.innerHTML += `<div class="single_cell cell3 fs-3 d-flex justify-content-center align-items-center">${i}</div>`
-    }
-}}
+            containerEl.innerHTML += `<div class="single_cell cell3 fs-3 d-flex justify-content-center align-items-center">${i}</div>`;
+        }
+    }}
 
-buttonEl.addEventListener("click", function () {
-    containerEl.innerHTML = "";
-    if (selectEl.value == "easy_lv") {
-        difficulty(easy);
-        const arrayCell = document.querySelectorAll(".single_cell")
-        changeBackground(arrayCell)
-    } else if (selectEl.value == "medium_lv") {
-        difficulty(medium);
-        const arrayCell = document.querySelectorAll(".single_cell")
-        changeBackground(arrayCell)
-    } else {
-        difficulty(hard);
-        const arrayCell = document.querySelectorAll(".single_cell")
-        changeBackground(arrayCell)
+    function changeBackground(arrayElement) {
+        for (let i = 0; i < arrayElement.length; i++) {
+            const singleCell = arrayElement[i]
+            singleCell.addEventListener("click", function () {
+                this.classList.toggle("click_background")
+                console.log(this.innerText)
+            })
+        }
     }
-})
 
-function changeBackground(arrayElement){
-for (let i = 0; i < arrayElement.length; i++) {
-    const singleCell = arrayElement[i]
-    singleCell.addEventListener("click", function(){
-        this.classList.toggle("click_background")
-        console.log(this.innerText)
+    buttonEl.addEventListener("click", function () {
+        containerEl.innerHTML = "";
+        if (selectEl.value == "easy_lv") {
+            difficulty(easy);
+        } else if (selectEl.value == "medium_lv") {
+            difficulty(medium);
+        } else {
+            difficulty(hard);
+        }
+        const arrayCell = document.querySelectorAll(".single_cell")
+        changeBackground(arrayCell)
     })
-}}
+
+
+// Animation explosion
+
+const imgContainer = document.getElementById("img_container");
+
+function getRandomPosition() {
+    const x = window.innerWidth - 175; 
+    const randomX = Math.floor(Math.random() * x);
+    return [randomX];
+}
+
+function showImage() {
+    imgContainer.style.opacity = 1;
+    const x = getRandomPosition();
+    imgContainer.style.left = x + "px";   
+}
+
+function hideImage() {
+    imgContainer.style.opacity = 0;
+}
+
+
+let showInterval = setInterval(showImage, 3000);
+let hideInterval = setInterval(hideImage, 2000);
 
 
 
